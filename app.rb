43580a524593b2e86561678e9964b3cb71f66a37
Sandro Padin <sandropadin@gmail.com>
2013-05-30 12:00:58 -0500

    updating

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'pusher'
+
+Pusher.app_id = '45285'
+Pusher.key = '898d03e2953cbf6d85c1'
+Pusher.secret = 'ae065864ed41591fd0fb'
 
 get '/test' do
   "Test"
+end
+
+get '/color' do
+  erb :color
+end
+
+post '/choose-color' do
+  color = params[:color]
+  Pusher.trigger('color', 'choose', {color: color})
 end
